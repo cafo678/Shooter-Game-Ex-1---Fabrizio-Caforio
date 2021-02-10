23d754af7baa9f631e92a851e28674c372832a45
cafo678 <caforio.f95@gmail.com>
2021-02-10 22:10:06 +0100

    Setting Movement Component

--- Source/ShooterGame/Private/My_ShooterCharacterMovement.cpp
+++ Source/ShooterGame/Private/My_ShooterCharacterMovement.cpp
@@ -1,7 +1,46 @@
 #include "ShooterGame.h"
 #include "My_ShooterCharacterMovement.h"
 
+// Function called from the input binding in the character
 void UMy_ShooterCharacterMovement::Teleport()
 {
-	UE_LOG(LogTemp, Warning, TEXT("Teleport"));
+	// Calcolate the Location to teleport to
+	TeleportLocation = PawnOwner->GetActorLocation() + PawnOwner->GetActorForwardVector() * TeleportOffset;
+	
+	// If we are the client and we have control we send the location to the server 
+	if (PawnOwner->Role == ENetRole::ROLE_AutonomousProxy) 
+		Server_SendTeleportLocation_Validate(TeleportLocation);
+
+	// This boolean will trigger the movement in the OnMovementUpdated function native of the standard Movement Component
+	bWantsToTeleport = true;
+}
+
+// Here we can validate the location of the teleport to prevent cheating
+bool UMy_ShooterCharacterMovement::Server_SendTeleportLocation_Validate(FVector LocationToTeleport)
+{
+    return true;
+}
+
+// Here we save the location sent in the local variable
+void UMy_ShooterCharacterMovement::Server_SendTeleportLocation_Implementation(FVector LocationToTeleport)
+{
+    TeleportLocation = LocationToTeleport;
+}
+
+// Function native of the standard Movement Component, this function is triggered at the end of a movement update.
+void UMy_ShooterCharacterMovement::OnMovementUpdated(float DeltaTime, const FVector& OldLocation, const FVector& OldVelocity)
+{
+	if (!CharacterOwner)
+		return;
+
+	// If the player has pressed the teleport key
+	if (bWantsToTeleport)
+	{
+		bWantsToTeleport = false;
+
+		// Sweep so we avoid collisions
+		CharacterOwner->SetActorLocation(TeleportLocation, true);
+	}
+
+	Super::OnMovementUpdated(DeltaTime, OldLocation, OldVelocity);
 }
--- Source/ShooterGame/Public/My_ShooterCharacterMovement.h
+++ Source/ShooterGame/Public/My_ShooterCharacterMovement.h
@@ -10,6 +10,25 @@ class SHOOTERGAME_API UMy_ShooterCharacterMovement : public UShooterCharacterMov
 	GENERATED_BODY()
 
 public:
+	// Function called from the input binding in the character
 	void Teleport();
+
+private:
+	// RPC that will execute on the server, sending the location to which the character will teleport. We can validate if we want.
+	UFUNCTION(Reliable, Server, WithValidation)
+	void Server_SendTeleportLocation(FVector LocationToTeleport);
+
+	// Function native of the standard Movement Component, this function is triggered at the end of a movement update.
+	void OnMovementUpdated(float DeltaTime, const FVector& OldLocation, const FVector& OldVelocity) override;
+
+	// Location to which the character will teleport
+	FVector TeleportLocation;
+
+	// We want to teleport 10m forward
+	float TeleportOffset = 1000; 
+
+	// This boolean will trigger the movement in the OnMovementUpdated function native of the standard Movement Component
+	// We use a single bit of a byte to fit this boolean in the FSavedMove_Character template we will override
+	uint8 bWantsToTeleport : 1;
 	
 };
